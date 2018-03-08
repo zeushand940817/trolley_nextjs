3173b4d317560d8d00461a727ecafd78ff94915c
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2018-03-08 15:58:35 -0300

    build env

--- next.config.js
+++ next.config.js
@@ -4,5 +4,12 @@ module.exports = {
           'marzipano': 'Marzipano'
       }
     return config
+  },
+  exportPathMap: function() {
+  	return {
+  		'/': { page: '/' },
+  		'/about': { page: '/about' },
+  		'/vista': { page: '/vista' }
+  	}
   }
 }
