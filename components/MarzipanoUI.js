b0c2922e9c0791d0b0cafc95760b88d7fd27dfaa
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2018-05-24 16:31:50 -0400

    ajustes para gyro

--- components/MarzipanoUI.js
+++ components/MarzipanoUI.js
@@ -1,74 +1,114 @@
-
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';	
-import faPlay from '@fortawesome/fontawesome-free-solid/faPlay';
-import faStop from '@fortawesome/fontawesome-free-solid/faStop';
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import faPlay from "@fortawesome/fontawesome-free-solid/faPlay";
+import faStop from "@fortawesome/fontawesome-free-solid/faStop";
+import faCompass from "@fortawesome/fontawesome-free-solid/faCompass";
 
 class MarzipanoUI extends React.Component {
 	constructor(props) {
-		super(props)
+		super(props);
 	}
 
 	render() {
 		const checkIcon = () => {
-			if(this.props.autorotate === true) {
-				return (<FontAwesomeIcon icon={faStop} />)
+			if (this.props.autorotate === true) {
+				return <FontAwesomeIcon icon={faStop} />;
 			} else {
-				return (<FontAwesomeIcon icon={faPlay} />)
+				return <FontAwesomeIcon icon={faPlay} />;
 			}
-		}
-		return(
+		};
+		return (
 			<div>
-			<div className={this.props.autorotate === true ? 'button active' : 'button'} onClick={this.props.rotate}>
-				{checkIcon()}
+				<div
+					className={
+						this.props.autorotate === true
+							? "button active"
+							: "button"
+					}
+					onClick={this.props.rotate}
+				>
+					{checkIcon()}
+				</div>
+				<div
+					className={
+						this.props.gyro === true
+							? "button-gyro active"
+							: "button-gyro"
+					}
+					onClick={this.props.gyro}
+				>
+					<FontAwesomeIcon icon={faCompass} />
+				</div>
+				<div className="sceneSwitcher">
+					{this.props.scenes.map(scene => (
+						<div
+							key={scene.id}
+							className={
+								this.props.activeScene === scene.id
+									? "sceneButton active"
+									: "sceneButton"
+							}
+							onClick={() =>
+								this.props.switcher(scene.scene, scene.id)
+							}
+							title={scene.title}
+						>
+							{scene.id}
+						</div>
+					))}
+				</div>
+				<style jsx>{`
+					.button {
+						position: absolute;
+						bottom: 0;
+						left: 10px;
+						background-color: white;
+						color: black;
+						padding: 6px;
+						z-index: 10;
+						font-size: 20px;
+						cursor: pointer;
+					}
+
+					.button-gyro {
+						position: absolute;
+						bottom: 0;
+						left: 60px;
+						background-color: white;
+						color: black;
+						padding: 6px;
+						z-index: 10;
+						font-size: 20px;
+						cursor: pointer;
+					}
+
+					.button:hover {
+						background-color: black;
+						color: white;
+					}
+					.sceneSwitcher {
+						position: absolute;
+						left: 120px;
+						bottom: 0;
+					}
+					.sceneButton {
+						padding: 6px 12px;
+						font-family: "Barrio", sans-serif;
+						font-size: 32px;
+						display: inline-block;
+						background-color: white;
+						text-align: center;
+						color: black;
+						margin-right: 6px;
+						cursor: pointer;
+					}
+					.sceneButton.active {
+						background-color: #e34f35;
+						color: white;
+					}
+				`}</style>
 			</div>
-			<div className="sceneSwitcher">
-				{this.props.scenes.map((scene) => (
-					<div key={scene.id} className={this.props.activeScene === scene.id ? 'sceneButton active' : 'sceneButton'} onClick={ () => this.props.switcher(scene.scene, scene.id) } title={scene.title}>{scene.id}</div>
-				))}
-			</div>
-			<style jsx>{
-				`
-				.button {
-					position: absolute;
-					bottom:0;
-					left:10px;
-					background-color:white;
-					color:black;
-					padding:6px;
-					z-index:10;
-					font-size:20px;
-					cursor: pointer;
-				}
-				.button:hover {
-					background-color:black;
-					color:white;
-				}
-				.sceneSwitcher {
-					position: absolute;
-					left:50px;
-					bottom:0;
-					
-				}
-				.sceneButton {
-					padding:6px 12px;
-					font-family:'Barrio', sans-serif;
-					font-size:32px;
-					display:inline-block;
-					background-color:white;
-					text-align:center;
-					color:black;
-					margin-right:6px;
-					cursor: pointer;
-				}
-				.sceneButton.active {
-					background-color:#E34F35;
-					color:white;
-				}
-				`
-			}</style>
-			</div>
-			)
+		);
 	}
 }
 
-export default MarzipanoUI
+export default MarzipanoUI;
--- components/MarzipanoView.js
+++ components/MarzipanoView.js
@@ -1,8 +1,8 @@
-import fetch from 'isomorphic-unfetch';
-import config from '../config.js';
-import Hotspot from '../components/Hotspot.js';
-import PointsList from '../components/PointsList.js';
-import MarzipanoUI from '../components/MarzipanoUI.js';
+import fetch from "isomorphic-unfetch";
+import config from "../config.js";
+import Hotspot from "../components/Hotspot.js";
+import PointsList from "../components/PointsList.js";
+import MarzipanoUI from "../components/MarzipanoUI.js";
 
 class MarzipanoView extends React.Component {
   constructor(props) {
@@ -16,41 +16,47 @@ class MarzipanoView extends React.Component {
       hotspotType: null,
       isRotating: false,
       autorotate: null,
+      isGyroOn: false,
       viewer: null,
+      view: null,
+      deviceControl: null,
+      controls: null,
       goTo: {
         yaw: 0,
         pitch: 0
       }
-    }
+    };
   }
 
   componentDidMount() {
-      this.startMarzipano();
-      console.log(this.props);
+    this.startMarzipano();
+    console.log(this.props);
   }
 
-  componentDidUpdate() {
-  }
+  componentDidUpdate() {}
 
   componentDidUpdate(prevProps, prevState) {
     //Movement updates
-    if(prevState.goTo.yaw !== this.state.goTo.yaw) {
+    if (prevState.goTo.yaw !== this.state.goTo.yaw) {
       let curscene = this.findScene(this.state.scenes, this.state.scene);
       console.log(curscene);
       curscene.scene.lookTo(this.state.goTo);
     }
-    if(prevState.isRotating !== this.state.isRotating) {
-      if(this.state.isRotating === false) {
+    if (prevState.isRotating !== this.state.isRotating) {
+      if (this.state.isRotating === false) {
         this.state.viewer.stopMovement();
       }
-      if(this.state.isRotating === true) {
-       this.state.viewer.startMovement(this.state.autorotate);
+      if (this.state.isRotating === true) {
+        this.state.viewer.startMovement(this.state.autorotate);
       }
     }
 
     //Scene updates
-    if(prevState.scene !== this.state.scene) {
-      this.setState({ curHotspots: this.findScene(this.state.scenes, this.state.scene).hotspots})
+    if (prevState.scene !== this.state.scene) {
+      this.setState({
+        curHotspots: this.findScene(this.state.scenes, this.state.scene)
+          .hotspots
+      });
     }
   }
 
@@ -58,25 +64,25 @@ class MarzipanoView extends React.Component {
     //Destroy marzipano
   }
 
-
   startMarzipano() {
-    const script = document.createElement('script');
+    const script = document.createElement("script");
     script.src = "./static/marzipano.js";
 
-
     script.onload = () => {
-      
-
       let panoElement = this.divContainer;
       // Create viewer.
       let viewer = new Marzipano.Viewer(panoElement);
+
+      //Start custom control
+      this.startDeviceControl();
+
       let autorotate = Marzipano.autorotate({
-        yawSpeed: 0.1,         // Yaw rotation speed
-        targetPitch: 0,        // Pitch value to converge to
-        targetFov: Math.PI/2   // Fov value to converge to
+        yawSpeed: 0.1, // Yaw rotation speed
+        targetPitch: 0, // Pitch value to converge to
+        targetFov: Math.PI / 2 // Fov value to converge to
       });
 
-      let scenes = this.props.data.scenes.map((scene) => {
+      let scenes = this.props.data.scenes.map(scene => {
         return this.createScene(scene, viewer);
       });
 
@@ -88,93 +94,61 @@ class MarzipanoView extends React.Component {
 
       //Start with first scene
       scenes[this.state.scene].scene.switchTo();
-      this.setState({scene: scenes[this.state.scene].id});
-    }
+      this.setState({ scene: scenes[this.state.scene].id });
+    };
 
     document.body.appendChild(script);
-    this.setState({loaded: true});
+    this.setState({ loaded: true });
   }
 
-rotateEuler(euler, result) {
-  var heading, bank, attitude,
-    ch = Math.cos(euler.yaw),
-    sh = Math.sin(euler.yaw),
-    ca = Math.cos(euler.pitch),
-    sa = Math.sin(euler.pitch),
-    cb = Math.cos(euler.roll),
-    sb = Math.sin(euler.roll),
+  startDeviceControl() {
+    const control = document.createElement("script");
+    control.src = "./static/devicecontrol.js";
 
-    matrix = [
-      sh*sb - ch*sa*cb,   -ch*ca,    ch*sa*sb + sh*cb,
-      ca*cb,              -sa,      -ca*sb,
-      sh*sa*cb + ch*sb,    sh*ca,   -sh*sa*sb + ch*cb
-    ]; // Includes 90-degree rotation around z axis
+    control.onload = () => {
+      this.setState({ deviceControl: new DeviceOrientationControlMethod() });
+      let controls = this.state.viewer.controls();
+      controls.registerMethod("deviceOrientation", this.state.deviceControl);
+    };
 
-  /* [m00 m01 m02] 0 1 2
-   * [m10 m11 m12] 3 4 5
-   * [m20 m21 m22] 6 7 8 */
-
-  if (matrix[3] > 0.9999)
-  {
-    // Deal with singularity at north pole
-    heading = Math.atan2(matrix[2],matrix[8]);
-    attitude = Math.PI/2;
-    bank = 0;
+    document.body.appendChild(control);
   }
-  else if (matrix[3] < -0.9999)
-  {
-    // Deal with singularity at south pole
-    heading = Math.atan2(matrix[2],matrix[8]);
-    attitude = -Math.PI/2;
-    bank = 0;
-  }
-  else
-  {
-    heading = Math.atan2(-matrix[6],matrix[0]);
-    bank = Math.atan2(-matrix[5],matrix[4]);
-    attitude = Math.asin(matrix[3]);
-  }
-
-  result.yaw = heading;
-  result.pitch = attitude;
-  result.roll = bank;
-}
 
   createScene(scene, viewer) {
+    // Create source.
+    let source = Marzipano.ImageUrlSource.fromString(scene.tilesurl);
 
-      // Create source.
-      let source = Marzipano.ImageUrlSource.fromString(scene.tilesurl);
+    // Create geometry.
+    let geometry = new Marzipano.CubeGeometry([{ tileSize: 2000, size: 2000 }]);
 
-      // Create geometry.
-      let geometry = new Marzipano.CubeGeometry([{ tileSize: 2000, size: 2000}]);
+    // Create view.
+    let limiter = Marzipano.RectilinearView.limit.traditional(
+      4096,
+      100 * Math.PI / 180
+    );
+    let view = new Marzipano.RectilinearView(
+      { yaw: 4 * Math.PI / 180, pitch: 0, fov: 1.2 },
+      limiter
+    );
 
-      // Create view.
-      let limiter = Marzipano.RectilinearView.limit.traditional(
-        4096,
-        100 * Math.PI / 180,
-      );
-      let view = new Marzipano.RectilinearView({yaw: 4 * Math.PI / 180, pitch: 0, fov: 1.2}, limiter);
+    // Create scene.
+    let curscene = viewer.createScene({
+      source: source,
+      geometry: geometry,
+      view: view,
+      pinFirstLevel: true
+    });
 
-      // Create scene.
-      let curscene = viewer.createScene({
-          source: source,
-          geometry: geometry,
-          view: view,
-          pinFirstLevel: true
-      });
+    this.setState({ view: view });
 
-      
-      // Display scene.
-      //scene.switchTo();
-      //console.log(curscene);
-      return {
-        scene: curscene,
-        view: view,
-        title: scene.title,
-        id: scene.id,
-        hotspots: scene.hotspots,
-        active: false
-      };
+    return {
+      scene: curscene,
+      view: view,
+      title: scene.title,
+      id: scene.id,
+      hotspots: scene.hotspots,
+      active: false
+    };
   }
 
   findScene(scenes, sceneid) {
@@ -183,28 +157,45 @@ rotateEuler(euler, result) {
 
   switchScene(scene, id) {
     this.findScene(this.state.scenes, id).scene.switchTo();
-    this.setState({scene: id});
+    this.setState({ scene: id });
   }
 
   toggleRotate() {
-     if(this.state.isRotating === true) {
-        this.setState({
-          isRotating: false
-        })
-     } else {
-      this.setState({isRotating: true})
-     }
+    if (this.state.isRotating === true) {
+      this.setState({
+        isRotating: false
+      });
+    } else {
+      this.setState({ isRotating: true });
+    }
+  }
+
+  toggleGyro() {
+    if (this.state.isGyroOn === true) {
+      this.setState({
+        isGyroOn: false
+      });
+    } else {
+      this.state.deviceControl.getPitch = (err, pitch) => {
+        if (!err) {
+          this.state.view.setPitch(pitch);
+        }
+      };
+      let controls = this.state.viewer.controls();
+      controls.enableMethod("deviceOrientation");
+      this.setState({ isGyroOn: true });
+    }
   }
 
   getCursorPosition(canvas, event) {
     var rect = canvas.getBoundingClientRect();
     var x = event.clientX - rect.left;
     var y = event.clientY - rect.top;
-    return {x: x, y: y};
+    return { x: x, y: y };
   }
 
-  handleClick(e) { 
-    if(process.env.NODE_ENV !== 'production') {
+  handleClick(e) {
+    if (process.env.NODE_ENV !== "production") {
       let position = this.getCursorPosition(this.divContainer, e);
       let curScene = this.findScene(this.state.scenes, this.state.scene);
       let coords = curScene.view.screenToCoordinates(position);
@@ -220,7 +211,7 @@ rotateEuler(euler, result) {
     this.setState({
       activeKey: 0,
       hotspotType: null
-    })
+    });
   }
 
   setPos(position, hotspotid, type) {
@@ -232,69 +223,81 @@ rotateEuler(euler, result) {
       activeKey: hotspotid,
       isRotating: false,
       hotspotType: type
-    })
+    });
   }
 
   render() {
     const panoStyle = {
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: "100%",
-              height: "100%",
-              overflow: "hidden"
-            }
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      overflow: "hidden"
+    };
 
-    if(this.state.loaded === true) {
-
-    return (
-      <div>
-        <div style={panoStyle}
-          id="panorama"
-          ref={container => {
-            this.divContainer = container;
-          }}
-          onClick={this.handleClick.bind(this)}
-        />
-        {this.state.curHotspots.map((hotspot) => (
-          <Hotspot  
-                    type={hotspot.type}
-                    active={this.state.activeKey === hotspot.id? true : false}
-                    onClick={this.hpState.bind(this, hotspot.id, hotspot.position, hotspot.type)}
-                    close={this.close.bind(this, hotspot.id)} 
-                    scene={this.findScene(this.state.scenes, this.state.scene)} 
-                    key={hotspot.id} 
-                    title={hotspot.title} 
-                    content={hotspot.content}
-                    data={hotspot.data} 
-                    keyword={hotspot.keyword} 
-                    position={hotspot.position}/>
+    if (this.state.loaded === true) {
+      return (
+        <div>
+          <div
+            style={panoStyle}
+            id="panorama"
+            ref={container => {
+              this.divContainer = container;
+            }}
+            onClick={this.handleClick.bind(this)}
+          />
+          {this.state.curHotspots.map(hotspot => (
+            <Hotspot
+              type={hotspot.type}
+              active={this.state.activeKey === hotspot.id ? true : false}
+              onClick={this.hpState.bind(
+                this,
+                hotspot.id,
+                hotspot.position,
+                hotspot.type
+              )}
+              close={this.close.bind(this, hotspot.id)}
+              scene={this.findScene(this.state.scenes, this.state.scene)}
+              key={hotspot.id}
+              title={hotspot.title}
+              content={hotspot.content}
+              data={hotspot.data}
+              keyword={hotspot.keyword}
+              position={hotspot.position}
+            />
           ))}
 
-          <PointsList 
-                  activeKey={this.state.activeKey}
-                  hotspots={this.state.curHotspots}
-                  setPos={this.setPos.bind(this)}
-                  hotspotType={this.state.hotspotType}
-                  
+          <PointsList
+            activeKey={this.state.activeKey}
+            hotspots={this.state.curHotspots}
+            setPos={this.setPos.bind(this)}
+            hotspotType={this.state.hotspotType}
           />
-          <MarzipanoUI scenes={this.state.scenes} autorotate={this.state.isRotating} rotate={this.toggleRotate.bind(this)} activeScene={this.state.scene} switcher={this.switchScene.bind(this)}/>
-      </div>
-    );
-  } else {
-    return (
-    <div> 
-      <div
-          style={panoStyle}
-          id="panorama"
-          ref={container => {
-            this.divContainer = container;
-          }}
-        />
-      <div>Cargando...</div>
+          <MarzipanoUI
+            scenes={this.state.scenes}
+            autorotate={this.state.isRotating}
+            rotate={this.toggleRotate.bind(this)}
+            activeScene={this.state.scene}
+            switcher={this.switchScene.bind(this)}
+            gyro={this.toggleGyro.bind(this)}
+          />
         </div>
       );
-  }
+    } else {
+      return (
+        <div>
+          <div
+            style={panoStyle}
+            id="panorama"
+            ref={container => {
+              this.divContainer = container;
+            }}
+          />
+          <div>Cargando...</div>
+        </div>
+      );
+    }
   }
 }
 
