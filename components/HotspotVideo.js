b436381815543e0f05fa7adea1bac26743644f02
zeushand940817 <zeushand940817@gmail.com>
2018-07-31 19:38:00 -0400

    funcionalidad videos
    Former-commit-id: 1efbdc813b44cb14c210298c9b95751475693b25

--- components/HotspotVideo.js
+++ components/HotspotVideo.js
@@ -1,5 +1,7 @@
 import config from "../config.js";
-import { Player } from "video-react";
+import { Player, LoadingSpinner, BigPlayButton, PlayToggle, ControlBar } from "video-react";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import faStepForward from "@fortawesome/fontawesome-free-solid/faStepForward";
 import data_trolley from "../data/data_trolley_dspace.json";
 
 class HotspotVideo extends React.Component {
@@ -9,6 +11,7 @@ class HotspotVideo extends React.Component {
       isActive: false,
       isLoaded: false,
       curVideo: null,
+      curVideoInfo: null,
       error: null,
       items: []
     };
@@ -17,12 +20,20 @@ class HotspotVideo extends React.Component {
   }
 
   componentDidMount() {
-    //Create hotspot function
-    //console.log(this.hpDiv)
     this.createHotspot(this.hpDiv, this.props.position);
     this.setState({
       curVideo: 0
     });
+  }
+
+  findVidInfo(id) {
+    let info;
+    for (let i = 0; i < data_trolley.length; i++) {
+      if (data_trolley[i]["dc.identifier.other"] === id) {
+        info = data_trolley[i];
+      }
+    }
+    return info["dc.title"];
   }
 
   componentWillReceiveProps() {
@@ -46,9 +57,31 @@ class HotspotVideo extends React.Component {
     }
   }
 
+  getCurVidId() {
+    if (this.state.curVideo !== null) {
+      let id = this.props.content.videos[this.state.curVideo].id;
+      return id;
+    }
+  }
+
+  getCurVidTitle() {
+    if (this.state.curVideo !== null) {
+      let title = this.props.content.videos[this.state.curVideo].title;
+      return title;
+    }
+  }
+
   getCurVid() {
     if (this.state.curVideo !== null) {
+      let id = this.props.content.videos[this.state.curVideo].id;
       return this.props.content.videos[this.state.curVideo].url;
+    }
+  }
+
+  getCurVidPoster() {
+    if (this.state.curVideo !== null) {
+      let id = this.props.content.videos[this.state.curVideo].id;
+      return './static/video/' + id + '.png';
     }
   }
 
@@ -69,9 +102,19 @@ class HotspotVideo extends React.Component {
             }
           >
             <div className="videoContainer">
-              <Player src={this.getCurVid()} playsInline />
+              <Player src={this.getCurVid()} playsInline fluid={false} preload="none" poster={this.getCurVidPoster()} width={900} height={670} >
+                <LoadingSpinner />
+                <BigPlayButton position="center" />
+                <ControlBar autoHide={false} disableDefaultControls={true}>
+                    <PlayToggle />
+                </ControlBar>
+              </Player>
+              <h1 className="vidtitle">{this.getCurVidTitle()}</h1>
               <span className="nextvid nav_vids" onClick={this.nextVid}>
-                Siguiente
+                <FontAwesomeIcon
+                className="fa-fw"
+                icon={faStepForward}
+              />
               </span>
             </div>
           </div>
@@ -86,8 +129,18 @@ class HotspotVideo extends React.Component {
               height: 650px;
               padding: 24px;
             }
+            h1.vidtitle {
+              font-size: 48px;
+              text-align: center;
+              font-family: 'Barrio', sans-serif;
+              margin-bottom:0;
+            }
             .nav_vids {
-              font-size: 72px;
+              font-size: 36px;
+              text-align:center;
+              display:block;
+              padding: 12px 0;
+              color: #e34f35;
             }
             .hpcontent {
               position: relative;
