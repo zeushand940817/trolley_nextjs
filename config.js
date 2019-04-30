d872b4f6673b0bdf7027220422b09cbc252d152d
zeushand940817 <zeushand940817@gmail.com>
2019-04-30 10:21:48 -0400

    cambio a http

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const config = {
 	project_name: "El Trolley",
 	project_shortdesc: "Archivo interactivo UC",
 	project_intro: "Visualización interactiva archivo Trolley",
-	baseurl: "https://eltrolley.cl",
-	dataurl: "https://static.eltrolley.cl/data/dummy.json",
-	assetsurl: "https://static.eltrolley.cl/",
-	trolleydataurl: "https://static.eltrolley.cl/data_trolley.json",
+	baseurl: "http://eltrolley.cl",
+	dataurl: "http://static.eltrolley.cl/data/dummy.json",
+	assetsurl: "http://static.eltrolley.cl/",
+	trolleydataurl: "http://static.eltrolley.cl/data_trolley.json",
 	dspacedataurl:
 		"http://146.155.39.134:8080/rest/collections/51fc4f59-dd6f-4106-94ca-47f3927a63ec?expand=all",
 	dspaceKeywordSeparator: "||"
